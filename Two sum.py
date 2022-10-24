17748f02d1d5dcc550394ead67a780e7bdf170a4
Vaughnski <37229177+Vaughnski@users.noreply.github.com>
2022-10-24 08:50:11 -0500

    Add files via upload

---
+++ Two sum.py
@@ -0,0 +1,5 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for num in range(len(nums)):
+            if (nums[num] + nums[num+1]):
+                return [num, num+1]
