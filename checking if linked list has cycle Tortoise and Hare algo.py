520da4e30663ef997a5eac81819f345355c0e497
Vaughnski <37229177+Vaughnski@users.noreply.github.com>
2022-11-13 15:51:47 -0600

    Add files via upload

---
+++ checking if linked list has cycle Tortoise and Hare algo.py
@@ -0,0 +1,9 @@
+try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
