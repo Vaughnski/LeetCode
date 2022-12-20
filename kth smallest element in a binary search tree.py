c48ff9d86e3eab62266d89baa171820feca1007a
Vaughnski <37229177+Vaughnski@users.noreply.github.com>
2022-12-20 12:24:01 -0600

    Add files via upload

---
+++ kth smallest element in a binary search tree.py
@@ -0,0 +1,10 @@
+stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
