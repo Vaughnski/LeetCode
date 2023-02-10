d7b02c1aa2efd903a7aa4fe068d5324ce8fe14d1
Vaughnski <37229177+Vaughnski@users.noreply.github.com>
2023-02-10 14:13:40 -0600

    Add files via upload

---
+++ merge intervals.py
@@ -0,0 +1,8 @@
+intervals.sort()
+        ans = [intervals[0]]
+        for i in intervals:
+            if i[0] <= ans[-1][1]:
+                ans[-1][1] = max(ans[-1][1], i[1])
+            else:
+                ans.append(i)
+        return ans
